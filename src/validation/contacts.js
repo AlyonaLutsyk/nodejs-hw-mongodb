a371bc2ec0f8efb1487d851f88ce35aad71dbf30
AlyonaLutsyk <148708226+AlyonaLutsyk@users.noreply.github.com>
2024-11-18 16:38:33 +0200

    finish-hw

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -4,13 +4,19 @@ import { contactTypeList } from "../constants/contacts.js";
 
 export const contactAddSchema = Joi.object({
     name: Joi.string().min(3).max(20).required().messages({
+        "string.min": "Name must be at least 3 characters long.",
+        "string.max": "Name must not exceed 20 characters.",
         "any.required": "Name is required.",
     }),
     phoneNumber: Joi.string().min(3).max(20).required().messages({
         "any.required": "Phone number is required.",
     }),
-    email: Joi.string().min(3).max(20).email().optional(),
-    isFavourite: Joi.boolean().optional(),
+    email: Joi.string().min(3).max(20).email().optional().messages({
+        "string.email": "Email must be a valid email address.",
+    }),
+    isFavourite: Joi.boolean().optional().messages({
+        "boolean.base": "isFavourite must be a boolean.",
+    }),
     contactType: Joi.string().valid(...contactTypeList),
 });
 
