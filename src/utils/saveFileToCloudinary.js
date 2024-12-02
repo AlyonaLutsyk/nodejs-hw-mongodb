dd71ebd648a851f5087ffd46620a7edb266c3516
AlyonaLutsyk <148708226+AlyonaLutsyk@users.noreply.github.com>
2024-12-02 10:52:46 +0200

    part-1

--- src/utils/saveFileToCloudinary.js
+++ src/utils/saveFileToCloudinary.js
@@ -5,9 +5,9 @@ import { env } from './env.js';
 
 cloudinary.v2.config({
   secure: true,
-  cloud_name: env("CLOUDINARY_CLOUD_NAME"),
-  api_key: env("CLOUDINARY_API_KEY"),
-  api_secret: env("CLOUDINARY_API_SECRET"),
+  cloud_name: env('CLOUDINARY_CLOUD_NAME'),
+  api_key: env('CLOUDINARY_API_KEY'),
+  api_secret: env('CLOUDINARY_API_SECRET'),
 });
 
 export const saveFileToCloudinary = async (file) => {
